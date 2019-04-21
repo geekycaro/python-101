10649fe72075c33e82c969ff766e241277b4d375
learning101 <learning101@learning101s-MacBook-Pro.local>
2019-04-21 19:17:20 -0400

    added new exercise

---
+++ Day2-Python/condition.py
@@ -0,0 +1,11 @@
+# create an empty distionary call 'days_i_poop'
+
+# Ask user for for what day do they poop and how many times (hint: you may need ask the user separately)
+
+# Populate the 'days_i_poop' dictionary with each day of the week as KEY
+
+# Populate the amount of time the user poop as integer as VALUE
+
+# print out entire 'days_i_poop' dictionay
+
+# Loop through the keys and value of 'days_i_poop' dictionay and print them individually
