4bf2c2d63b560356ceb751afef932653e0239a60
learning101 <learning101@learning101s-MacBook-Pro.local>
2019-04-21 21:34:00 -0400

    added hw

--- Day2-Python/condition.py
+++ Day2-Python/condition.py
@@ -1,8 +1,16 @@
 # create an empty distionary call 'days_i_poop'
 
-# Ask user for for what day do they poop and how many times (hint: you may need ask the user separately)
+days_i_poop = {}
+
+# Ask user for for what day do they poop and how many times 
+# (hint: you may need ask the user separately)
+
+days = input(" what day do you poop? ")
+poop = input(" how many times you pooped that day? ")
 
 # Populate the 'days_i_poop' dictionary with each day of the week as KEY
+
+days_i_poop[days] = int(poop)
 
 # Populate the amount of time the user poop as integer as VALUE
 
