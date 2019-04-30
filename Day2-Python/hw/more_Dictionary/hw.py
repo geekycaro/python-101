5f45f9ff9ecc712ae0cb8d4245cdc09e67e24d32
GeekyCaro <krollynyau@Krollyns-iMac.local>
2019-04-29 22:28:51 -0400

    finalized loop exercise

--- Day2-Python/hw/more_Dictionary/hw.py
+++ Day2-Python/hw/more_Dictionary/hw.py
@@ -16,34 +16,54 @@ ips = ["209.244.0.3", "64.6.64.6", "8.8.8.8", "9.9.9.9", "84.200.69.80",
 ### Part 1 - Provider Dictionary ###
 ####################################
 DNS_dictionary = {}
+# print(len(providers))
+# print(len(ips))
 
 # Use a for loop to create a dictionary mapping the provider names to their IPs. expected output: {'Level3': '209.244.0.3', ...}
-<YOUR CODE HERE>
+for dns in range(len(providers)):
+       DNS_dictionary[providers[dns]] = ips[dns]
+
+   
+
 
 print("DNS Dictionary: ")
 print(DNS_dictionary)
 print("--------")
 
-# Use the dictionary to print Hurricane Electric's IP
-print("Hurricane Electric's IP is: " + <YOUR CODE HERE>)
+# # Use the dictionary to print Hurricane Electric's IP
+print("Hurricane Electric's IP is: " + str(DNS_dictionary["Hurricane Electric"]))
+#print(DNS_dictionary["Hurricane Electric"])
 print("--------")
 print("--------")
 
 
-##################################
-### Part 2 - List of Providers ###
-##################################
-DNS_dictionaries = []
+# ##################################
+# ### Part 2 - List of Providers ###
+# ##################################
+sec_dictionaries = {}
+proly = []
 
-# Use a for loop to create a list of dictionaries with the associated information. expected output: [{'provider_name': 'Level3', 'primary_server': '209.244.0.3'}, ...]
-<YOUR CODE HERE>
+
+for a,b in DNS_dictionary.items():
+       sec_dictionaries = {}
+       sec_dictionaries["provider_name"] = a
+       sec_dictionaries["primary_server"] = b
+       proly.append(sec_dictionaries)
+
+
+
+# # Use a for loop to create a list of dictionaries with the associated information. expected output: [{'provider_name': 'Level3', 'primary_server': '209.244.0.3'}, ...]
+
 
 print("DNS Dictionaries: ")
-print(DNS_dictionaries)
+#print(proly[0]) used for when you want to get to a specific location inside the list through an index
+print(proly)
 print("--------")
 
-# Use the list to print the total number of providers
-print("Number of providers: " + <YOUR CODE HERE>)
+
+# # Use the list to print the total number of providers
+print("Number of providers: " + str(len(providers)))
 print("--------")
 print("--------")
 
+#print(len(providers))
