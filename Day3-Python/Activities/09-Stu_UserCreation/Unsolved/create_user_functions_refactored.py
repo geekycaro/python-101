24bb5c5fa48936a19ac3f29c5e7a44a32fa5a3b6
Baymax <baymax@Baymaxs-MacBook-Air.local>
2019-05-26 12:05:36 -0400

    new project

--- Day3-Python/Activities/09-Stu_UserCreation/Unsolved/create_user_functions_refactored.py
+++ Day3-Python/Activities/09-Stu_UserCreation/Unsolved/create_user_functions_refactored.py
@@ -14,13 +14,15 @@ def collect_user_information():
     username = input("What's your username? ")
 
     # Prompt the user for their email and store it in a variable called email
-    
+    email = input("What's your email? ")
    
     # Prompt the user for their password and store it in a variable called password
-    
+    password = input("What's your password? ")
     
     # Create a list that will store all of the user data collected inside of it and call it user_info
+    user_info = [username, email, password]
 
+    return user_info
 
     # Return the above list (called user_info)
     
@@ -33,6 +35,10 @@ def create_user(user_data):
 
     # Use your `validate_password` function to check if the password is secure
     if validate_password(password):
+        user = {"username" : username,
+        "email" : email,
+        "password" : password}
+
         # If yes, create a dictionary, called `user` with the keys "Username", "Email", and "Password" and associate these keys with the values the user entered
 
         
@@ -41,7 +47,10 @@ def create_user(user_data):
         
         # When you're done, use `items` method to print: `"Your username is <USERNAME>"`; `"Your email is <EMAIL"`; and `"Your password is <PASSWORD>"`
         for key, value in user.items():
-            print("Your " + key + " is: " + value + ".") 
+            print("Your " + key + " is: " + value + ".")
+        return user 
+    else:
+        print("Please try again, password is too short")
 
         # Return user
 
